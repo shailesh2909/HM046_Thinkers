571237fbb19b08433b5d5571a31eaa24f69eb6ee
vansh gaikwad <vanshgaikwad72@gmail.com>
2026-01-17 15:08:25 +0530

    -delete

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import "./Footer.css";
-
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <p>© 2026 SkillBridge. All rights reserved.</p>
-    </footer>
-  );
-}
